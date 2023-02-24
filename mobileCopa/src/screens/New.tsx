f674c9b71c69c2c824afaabf47cf8431cf8b4d09
John Santos <jhow.mito11@gmail.com>
2023-02-24 14:50:57 -0300

    Configurando toast

--- mobileCopa/src/screens/New.tsx
+++ mobileCopa/src/screens/New.tsx
@@ -1,12 +1,46 @@
-import { Center, Heading, Text, VStack } from "native-base";
+import { Center, Heading, Text, useToast, VStack } from "native-base";
 import Logo from '../assets/logo.svg'
 
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useState } from "react";
+
 
 
 export function New() {
+	const [title, setTitle] = useState('');
+	const [isLoading, setIsLoading] = useState(false)
+
+	const toast = useToast()
+
+	async function handlePoolCreate() {
+		console.log('uiii')
+		//title.trim() para verificar se tem espaços na aplicação
+		if (!title.trim()) {
+			return toast.show({
+				title: 'Informe um nome para o seu bolão!',
+				placement: 'top',
+				bgColor: 'red.500'
+			})
+		}
+		try {
+			setIsLoading(true)
+
+
+		} catch (error) {
+			console.log(error)
+			toast.show({
+				title: 'Não foi possivel criar o bolão',
+				placement: 'top',
+				bgColor: 'red.500'
+			})
+		} finally {
+			setIsLoading(false)
+		}
+
+	}
+
 	return (
 		<VStack flex={1} bgColor={"gray.900"} >
 			<Header title="Criar novo bolão" />
@@ -22,9 +56,13 @@ export function New() {
 				<Input
 					marginBottom={2}
 					placeholder="Qual nome do seu bolão?"
+					value={title}
+					onChangeText={setTitle}
 				/>
 				<Button
+					onPress={handlePoolCreate}
 					title="CRIAR MEU BOLÃO"
+					isLoading={isLoading}
 				/>
 
 				<Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4} >
